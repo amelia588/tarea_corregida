2f55fca88c0dbf1bbe29bbb14eaf7af6bfaeedf6
Amelia Martel <1473967@senati.pe>
2023-02-23 22:36:52 -0500

    se agrego la funcion producto y se visualiza con print

--- operaciones.py
+++ operaciones.py
@@ -7,3 +7,9 @@ suma =num1 + num2
 
 # mostrar el resultado de la suma
 print("la suma es", num1, "y", num2, "es igual a:", suma)
+
+# multiplicar los dos numeros
+producto =num1 * num2
+
+# mostrar el resultado de la multiplicacion
+print("el producto es", num1, "y", num2, "es igual a:", producto)
