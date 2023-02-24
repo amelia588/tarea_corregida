d6d002ff2d2be010a46ef19e08b8442471b18193
Amelia Martel <1473967@senati.pe>
2023-02-23 22:21:41 -0500

    python

---
+++ operaciones.py
@@ -0,0 +1,9 @@
+# pedir al usuario que ingrese los dos numeros a sumar
+num1 = float(input("ingrese el primer numero: "))
+num2 = float(input("ingrese el segundo numero: "))
+
+# sumar los dos numeros
+suma =num1 + num2
+
+# mostrar el resultado de la suma
+print("la suma es", num1, "y", num2, "es igual a:", suma)
