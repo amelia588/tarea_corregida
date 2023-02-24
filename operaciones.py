a01571d77a002af72829b37d63bc0186bd78cad9
Amelia Martel <1473967@senati.pe>
2023-02-23 22:48:45 -0500

    se agrego la funcion cociente y se visualiza con print

--- operaciones.py
+++ operaciones.py
@@ -13,3 +13,8 @@ producto =num1 * num2
 
 # mostrar el resultado de la multiplicacion
 print("el producto es", num1, "y", num2, "es igual a:", producto)
+
+# dividir los dos numeros
+cociente = num1 /num2
+# mostrar el resultado de la division
+print("el resultado de dividir",num1, "y",num2, "es igual a:", cociente)
