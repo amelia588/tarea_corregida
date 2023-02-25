200b855a5e1d1df9fdaf30ef4a0d93c7e6b8c029
Amelia Martel <1473967@senati.pe>
2023-02-24 23:25:13 -0500

    practicando con listas

---
+++ 5_list.py
@@ -0,0 +1,7 @@
+#Desarrollar un algoritmo y que muestre los numeros
+# divisibles en entre 5 de una lista de numeros.
+numbers=[10,20,30,45,50,55]
+for i in numbers:
+    if i % 5 ==0:
+        print(i)
+        
